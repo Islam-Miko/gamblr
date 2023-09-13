b13240afbde8a8fc79d44959ded9a0564070e741
miko <islamimanhod@gmail.com>
2023-09-13 23:19:43 +0600

    feat: create Event entity, configure settings, add redis utils

---
+++ line-provider/app/events.py
@@ -0,0 +1,25 @@
+import decimal
+import enum
+
+from pydantic import BaseModel, Field, field_serializer
+
+
+class EventState(enum.IntEnum):
+    NEW = 1
+    FINISHED_WIN = 2
+    FINISHED_LOSE = 3
+
+
+class Event(BaseModel):
+    event_id: int
+    coefficient: decimal.Decimal = Field(..., ge=0, decimal_places=2)
+    deadline: int
+    state: EventState
+
+    @field_serializer("coefficient")
+    def serializer_coefficient(self, coefficient: decimal.Decimal) -> str:
+        return str(coefficient)
+
+    @field_serializer("state")
+    def serializer_state(self, state: EventState) -> int:
+        return state.value
