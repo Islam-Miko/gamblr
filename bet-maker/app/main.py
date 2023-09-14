9397e63f72aa485f05e7490efc4df5d1ad91697a
miko <islamimanhod@gmail.com>
2023-09-14 21:18:08 +0600

    feat: configure redis connection

--- bet-maker/app/main.py
+++ bet-maker/app/main.py
@@ -1,10 +1,27 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+import redis.asyncio as aioredis
 from fastapi import FastAPI
 
+from . import redis
 from .routes import router
+from .settings import get_settings
 
-app = FastAPI(
-    title="Bet-maker",
-)
+SETTINGS = get_settings()
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI) -> AsyncGenerator:
+    pool = aioredis.ConnectionPool.from_url(
+        SETTINGS.REDIS_DSN, max_connections=10, decode_responses=True
+    )
+    redis.redis_client = aioredis.Redis(connection_pool=pool)
+    yield
+    await pool.disconnect()
+
+
+app = FastAPI(title="Bet-maker", lifespan=lifespan)
 
 
 @app.get("/health")
