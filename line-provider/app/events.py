1989538131a2cefb3c34295581bd71d11489be45
miko <islamimanhod@gmail.com>
2023-09-15 00:04:20 +0600

    fix: add validator to Event

--- line-provider/app/events.py
+++ line-provider/app/events.py
@@ -12,7 +12,7 @@ class EventState(enum.IntEnum):
 
 class Event(BaseModel):
     event_id: int
-    coefficient: decimal.Decimal = Field(..., ge=0, decimal_places=2)
+    coefficient: decimal.Decimal = Field(..., ge=0, decimal_places=2, le=1000)
     deadline: int
     state: EventState
 
